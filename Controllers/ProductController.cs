65987b2ae40fff5680aec91d2538455efc886ba4
Andy <andy2911131@gmail.com>
2023-08-12 16:01:06 +0800

    第一次

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Trash()
+        {
+            return View();
+        }
+    }
+}
